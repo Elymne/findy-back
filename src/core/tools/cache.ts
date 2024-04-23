7f2a8806beb7983011c8910b399133c12890fe76
Elymne <sacha.djurdjevic@gmail.com>
2024-04-23 17:17:03 +0200

    change : sample job offers has been changed to be more atomic

--- src/core/tools/cache.ts
+++ src/core/tools/cache.ts
@@ -3,10 +3,23 @@ import apicache from "apicache"
 
 const cache = apicache.middleware
 
+/**
+ * For daily job query.
+ */
 export const cache24hours = cache("24 hours", (req: Request, res: Response) => {
     return res.statusCode === 200 || res.statusCode === 206
 })
 
+/**
+ * For sample usage.
+ */
+export const cacheWeek = cache("7 days", (req: Request, res: Response) => {
+    return res.statusCode === 200 || res.statusCode === 206
+})
+
+/**
+ * For cities fetching but useless now because we don't use this anymore.
+ */
 export const cache5Mins = cache("5 minutes", (req: Request, res: Response) => {
     return res.statusCode === 200 || res.statusCode === 206
 })
--- src/routes/jobsOffers/getSample.route.ts
+++ src/routes/jobsOffers/getSample.route.ts
@@ -2,17 +2,28 @@ import express, { Request, Response } from "express"
 import { Failure } from "@App/core/interfaces/abstract.usecase"
 import GetSampleUsecase, { GetSampleUsecaseImpl } from "@App/domain/usecases/jobsOffers/getSample.usecase"
 import { cache24hours } from "@App/core/tools/cache"
+import { query, validationResult } from "express-validator"
 
 const getSampleUsecase: GetSampleUsecase = GetSampleUsecaseImpl
 
-const getSampleJobOffersRoute = express.Router().get("/sample", cache24hours, async (req: Request, res: Response) => {
-    const result = await getSampleUsecase.perform()
+const getSampleJobOffersRoute = express
+    .Router()
+    .get("/sample", query("categ").isString().notEmpty().escape(), cache24hours, async (req: Request, res: Response) => {
+        const validator = validationResult(req)
+        if (!validator.isEmpty()) {
+            res.status(404).send(validator)
+            return
+        }
 
-    if (result instanceof Failure) {
-        return res.status(result.errorCode).send(result)
-    }
+        const result = await getSampleUsecase.perform({
+            categ: req.query.keywords as string,
+        })
 
-    res.status(200).send(result)
-})
+        if (result instanceof Failure) {
+            return res.status(result.errorCode).send(result)
+        }
+
+        res.status(200).send(result)
+    })
 
 export default getSampleJobOffersRoute
