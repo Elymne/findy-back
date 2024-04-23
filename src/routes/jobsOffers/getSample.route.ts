8469f23e13d1a8e73da2c354c59d0b8a567aadda
Elymne <sacha.djurdjevic@gmail.com>
2024-04-23 17:41:52 +0200

    fix : add a longer cache for sample data

--- src/routes/jobsOffers/getSample.route.ts
+++ src/routes/jobsOffers/getSample.route.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from "express"
 import { Failure } from "@App/core/interfaces/abstract.usecase"
 import GetSampleUsecase, { GetSampleUsecaseImpl } from "@App/domain/usecases/jobsOffers/getSample.usecase"
-import { cache24hours } from "@App/core/tools/cache"
+import { cacheWeek } from "@App/core/tools/cache"
 import { query, validationResult } from "express-validator"
 
 const getSampleUsecase: GetSampleUsecase = GetSampleUsecaseImpl
 
 const getSampleJobOffersRoute = express
     .Router()
-    .get("/sample", query("categ").isString().notEmpty().escape(), cache24hours, async (req: Request, res: Response) => {
+    .get("/sample", query("categ").isString().notEmpty().escape(), cacheWeek, async (req: Request, res: Response) => {
         const validator = validationResult(req)
         if (!validator.isEmpty()) {
             res.status(404).send(validator)
